e1090fefa14eb60bddc8486960cf98a7bb0df73f
Instanes <instanes1@gmail.com>
2025-04-27 21:45:47 +0200

    B: Long over due commit. We got asteroids!

--- main.py
+++ main.py
@@ -4,17 +4,41 @@
 
 
 from constants import *
+from player import *
+from asteroidfield import *
+from asteroids import Asteroid
 import pygame
 
 
 def main():
     pygame.init()
     
+    
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")  # Should work with wildcard import
     print(f"Screen height: {SCREEN_HEIGHT}")  # Should work with wildcard import
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    #Groups
+    updatable = pygame.sprite.Group()
+    #drawable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+    
+    
+    
+    Player.containers = (updatable, drawable)
+    
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    #print(drawable.sprites())
+    dt = 0
+    
+    
+   
     
     # Add your game loop here
     while True:
@@ -22,7 +46,19 @@ def main():
             if event.type == pygame.QUIT:
                 return
             
-    screen.fill("black")
+        updatable.update(dt)
+        screen.fill("black")    
+        #player.update(dt)
+        
+        for obj in drawable:
+            obj.draw(screen)   
+        
+        #player.draw(screen)
+        pygame.display.flip()
+        
+
+    
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
