e8f4fff58a96dc32b217e6e1a2aa09a01f7066e5
Instanes <instanes1@gmail.com>
2025-05-02 13:25:26 +0200

    D: Asteroid deletio when hit by shot.

--- player.py
+++ player.py
@@ -7,6 +7,7 @@ class Player(CircleShape):
         
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.shoot_timer = 0
   
   
   # in the player class
@@ -23,6 +24,7 @@ class Player(CircleShape):
         self.rotation += turning
         
     def update(self, dt):
+        self.shoot_timer -= dt
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
@@ -48,6 +50,9 @@ class Player(CircleShape):
         self.position += (forward * PLAYER_SPEED * dt) * 2
         
     def shoot(self):
+        if self.shoot_timer > 0:
+            return
+        self.shoot_timer = PLAYER_SHOOT_COOLDOWN
         shot = Shot(self.position.x, self.position.y)
         shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOT_SPEED
         
