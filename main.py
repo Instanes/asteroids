e9459a1e6fd750cfecc8664b80a36e85189be225
Instanes <instanes1@gmail.com>
2025-05-04 10:56:57 +0200

    E: The final entry for now

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ def main():
              for shot in shots:
                 if asteroid.collides_with(shot):
                     shot.kill()
-                    asteroid.kill()
+                    asteroid.split()
         screen.fill("black")    
         #player.update(dt)
         
